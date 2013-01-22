1f0ff664373d5d9b54afcbf43c9801bcd1592ee3
jamesbloomer <github2@thebloomers.co.uk>
2013-01-22 17:30:54 +0000

    added simple call to ec2 to list instances

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var cec2ation = require('./lib/cec2ation.js');
 
-cec2ation.configure('0 0 * * * 1', '0 0 * * * 6');
+var config = { 
+	accessKeyId: process.env.AWS_ACCESS_KEY,
+	secretAccessKey: process.env.AWS_SECRET_KEY,
+	region: process.env.AWS_REGION,
+	instanceIds: []
+};
+
+cec2ation.configure('0 0 * * * 1', '0 0 * * * 6', config);
