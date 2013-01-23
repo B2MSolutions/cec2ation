5cbc3fbd1ec8bd582ba08f703e118b44696497fb
roylines <roy@roylines.co.uk>
2013-01-23 16:01:39 +0000

    set stop time to just after midnight on Friday

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var cec2ation = require('./lib/cec2ation.js');
 
-//stop: '01 00 00 * * 6',
 var config = { 
 	accessKeyId: process.env.AWS_ACCESS_KEY,
 	secretAccessKey: process.env.AWS_SECRET_KEY,
 	region: process.env.AWS_REGION,
-	stop: '00 50 15 * * *',
+	stop: '01 00 00 * * 6',
 	instances: {
 		Filters: [{
 			Name: 'tag:cec2ation',
