e9fff335318356424ecfe0496dd8792e6c6a3ba8
jamesbloomer <github2@thebloomers.co.uk>
2013-01-23 15:24:39 +0000

    testing timezones

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var cec2ation = require('./lib/cec2ation.js');
 
+//stop: '01 00 00 * * 6',
 var config = { 
 	accessKeyId: process.env.AWS_ACCESS_KEY,
 	secretAccessKey: process.env.AWS_SECRET_KEY,
 	region: process.env.AWS_REGION,
-	stop: '01 00 00 * * 6',
+	stop: '00 26 15 * * 6',
 	instances: {
 		Filters: [{
 			Name: 'tag:cec2ation',
