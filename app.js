6951e560d3623260d42d128a2d91e4fbd6d7b599
roylines <roy@roylines.co.uk>
2013-02-06 12:33:07 +0000

    changed to turn off each day at 2.00am

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var config = {
 	accessKeyId: process.env.AWS_ACCESS_KEY,
 	secretAccessKey: process.env.AWS_SECRET_KEY,
 	region: process.env.AWS_REGION,
-	stop: '01 00 00 * * 6',
+	stop: '00 00 02 * * *',
 	instances: {
 		Filters: [{
 			Name: 'tag:cec2ation',
-			Values: ['on during weekdays']
+			Values: ['off at 2am']
 		}]
 	}
 };
