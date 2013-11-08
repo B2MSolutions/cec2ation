b3f01d9ccbb9d84d05ebd94cef9f27c822009d2a
roylines <roy@roylines.co.uk>
2013-11-08 11:30:32 +0000

    added cron examples

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var cec2ation = require('./lib/cec2ation.js');
 
+/*
+
+* * * * *
+- - - - -
+| | | | |
+| | | | +----- day of week (0-6) (Sunday=0)
+| | | +------- month (1-12)
+| | +--------- day of month (1-31)
+| +----------- hour (0-23)
++------------- min (0-59)
+
+*/
+
 var config = { 
 	accessKeyId: process.env.AWS_ACCESS_KEY,
 	secretAccessKey: process.env.AWS_SECRET_KEY,
